10aa336cf4c42bcf4ef1f54f63adcf16191f57bb
Paula Borges <paulabzambeli@gmail.com>
2025-02-04 18:48:17 -0300

    feat:route

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import Footer from "../../components/footer/footer";
+import Header from "../../components/header/header";
+
+
+const HomePage = () => {
+  return (
+      <div className="w-full flex flex-col">
+          <Header />
+          <h1 className="bg-red-800">Testando!</h1>
+          <Footer />
+          
+         
+       
+      </div>
+  );
+};
+export default HomePage
---
+++ src/pages/page-route.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+
+export const PageRoute = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+};
