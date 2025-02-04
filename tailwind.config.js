fadb203c4f69e9867e6bd69a4ec4b1ff02990545
Paula Borges <paulabzambeli@gmail.com>
2025-02-04 18:49:27 -0300

    fix:config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        500: "500px",
+        1440: '1440px',
+      },
+      fontWeight: {
+        regular: 400,
+        bold: 700,
+        medium: 500,
+        semibold: 600,
+      },
+      lineHeight: {
+        '24': '24px',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
