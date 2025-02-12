55c06e7c751be77eba464d1dc8a790166f86c0be
Paula Borges <paulabzambeli@gmail.com>
2025-02-12 09:49:01 -0300

    fix:pruduct-detail-shop

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -9,8 +9,8 @@ import ProductsComponent from "./components/productsComponent";
 const Shop = () => {
     return (
       <div className=" flex flex-col">
-         <Header />
-         <Banner title="Shop" subtitle="Home > Shop" />
+         <Header />      
+         <Banner title="Shop" subtitle="Home > Shop" showLogo={false} />
          <Filter />
          <ProductsComponent />
          <Section4 />
